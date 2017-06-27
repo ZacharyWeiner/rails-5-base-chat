b41d789b024e27b72bbca6d6c3b9393330762b8f
ZacharyWeiner <zach@cloud.com>
2017-06-26 23:43:41 -0500

    added messages object, refactored chatroom routes, added messages model and partial, added gravitar helper method, added messages relationship to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'chat_rooms', to: 'chat_rooms#index'
-  get 'chat_rooms/new', to: 'chat_rooms#new'
-  post 'chat_rooms', to: 'chat_rooms#create'
-  get 'chat_rooms/:id', to: 'chat_rooms#show', as: 'chat_room_show'
+  resources :chat_rooms, only: [:new, :create, :show, :index]
+
+  root 'chat_rooms#index'
 end
